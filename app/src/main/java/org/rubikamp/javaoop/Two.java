23575fc0795a9b2d64d931059d3a4b7f82231cf2
Sana <sanaebadi97@gmail.com>
2022-01-30 20:42:29 +0330

    practiced encapsulation and sub and super class

---
+++ app/src/main/java/org/rubikamp/javaoop/Two.java
@@ -0,0 +1,13 @@
+package org.rubikamp.javaoop;
+
+public class Two extends One{
+
+    @Override
+    public void oneFun() {
+        super.oneFun();
+    }
+
+    public void  twoFun(){
+
+    }
+}
