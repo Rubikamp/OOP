26d94f062fa16dd0215b6e72c5b3f839e003fa93
Sana <sanaebadi97@gmail.com>
2022-01-30 20:43:06 +0330

    practiced encapsulation and sub and super class

---
+++ app/src/main/java/org/rubikamp/javaoop/One.java
@@ -0,0 +1,18 @@
+package org.rubikamp.javaoop;
+
+public class One {
+
+    private int count = 5;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void oneFun() {
+
+    }
+}
